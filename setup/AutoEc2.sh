a43122de219120f152fc1c30d2dec718bfc96ad2
Yousuf <ykanan2004@gmail.com>
2024-04-18 01:08:26 -0400

    Auto EC2 Maybe ...

---
+++ setup/AutoEc2.sh
@@ -0,0 +1,8 @@
+#The purposse of this script is to automate the creation of an EC2 instance in AWS 
+# then it will automtically download the necessary files to ssh into the instance
+# and then it will set up the necessary dependencies to run the project
+# concens are the following:
+# 1. The user has the necessary permissions to create get and amazon key for the instance
+# 2. automtically donwloading the necessary files to ssh into the instance may not be secure
+# 3. The user successfully sets up the necessary dependencies to run the project such as git and virtualenv ...
+
