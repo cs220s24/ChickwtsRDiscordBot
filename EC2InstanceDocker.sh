92bbe1cf420dd84c0d84b15494c628ec0b22513c
Saul Sanchez <sanchezs@moravian.edu>
2024-04-24 13:54:10 -0400

    modified EC2InstanceDocker script

--- EC2InstanceDocker.sh
+++ EC2InstanceDocker.sh
@@ -6,22 +6,28 @@ then
     echo "Docker is not installed. Installing Docker..."
     # Install Docker
     sudo yum install docker -y
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Docker."
+        exit 1
+    fi
 else
     echo "Docker is already installed."
 fi
 
 # Enable and start Docker
 sudo systemctl enable docker
-sudo systemctl start docker
+if [ $? -ne 0 ]; then
+    echo "Failed to enable Docker."
+    exit 1
+fi
 
-# Ensure the script is in the correct directory before proceeding
-if [ -d "../bot" ]; then
-    cd ../bot
-else
-    echo "Directory '../bot' does not exist. Exiting..."
+sudo systemctl start docker
+if [ $? -ne 0 ]; then
+    echo "Failed to start Docker."
     exit 1
 fi
 
 # Interact as ec2-user
 sudo usermod -a -G docker ec2-user
+
 
