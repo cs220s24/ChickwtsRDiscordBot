e5ed4a4b04b874101e8969279df54742b3695288
Saul Sanchez <sanchezs@moravian.edu>
2024-04-25 07:31:35 -0400

    fixed fonts

--- bot/generateBarplot.R
+++ bot/generateBarplot.R
@@ -9,6 +9,7 @@ par(family = "mono")
 
 # Generate a boxplot
 png("barplot_chickwts.png")  # Save the plot as a PNG file
-barplot(feed_counts, main = "Barplot of Chickwts Feed Types", xlab = "Feed Type", ylab = "Count")
+barplot(feed_counts, main = "Barplot of Chickwts Feed Types", xlab = "Feed Type", ylab = "Count", 
+cex.names = 0.7, cex.axis = 0.7)
 dev.off()  # Close the PNG device
 
--- bot/generateBoxplot.R
+++ bot/generateBoxplot.R
@@ -3,6 +3,7 @@ data("chickwts")
 
 # Generate a boxplot
 png("boxplot_chickwts.png")  # Save the plot as a PNG file
-boxplot(weight ~ feed, data = chickwts, main="Boxplot of Chicken Weights by Feed Type", xlab="Feed Type", ylab="Weight")
+boxplot(weight ~ feed, data = chickwts, main="Boxplot of Chicken Weights by Feed Type", xlab="Feed Type", ylab="Weight",
+cex.axis = 0.7, cex.names = 0.7)
 dev.off()  # Close the PNG device
 
